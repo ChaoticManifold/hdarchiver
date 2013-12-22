8c41b5ae7c912ba4b609bf1f22579edc28ba990d
blockynight <joncpim@gmail.com>
2013-12-22 19:18:08 +0000

    Trying to use branches.

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 -- A tool to pack and unpack .dar files, specification is found in the README.
 
 import Control.Applicative
+import Data.Binary
 import Data.Binary.Get          
 import Data.Binary.Put
 import Data.Int
@@ -24,6 +25,8 @@ instance Eq DarHeader where
                    && majorV darH1 == majorV darH2
                    && minorV darH1 == minorV darH2
 
+
+
 data DataHeader = DataH { identifyDATA :: B.ByteString --Should be ".data"
 -- not part of spec yet?, lengthDATA :: Word64
                         , dataNodes :: [DataNode] -- Not sure if this is best?
@@ -42,9 +45,6 @@ data DataNodeName = NodeLN { nodeNameLen :: Word32
 cDAR_IDENTIFICATION = BC.pack "DAR" :: B.ByteString
 cDAR_MAJOR_VERSION = 6 :: Word8
 cDAR_MINOR_VERSION = 0 :: Word8
--- cDAR_NODE_COUNT_TEST = 0 :: Word64 -- Should be removed?
--- cDAR_CHECKSUM_TEST = 0 :: Word32   -- Should be removed?
--- cDAR_HEADER_SIZE = 17
 cDAR_DATANODE_BLOCK = BC.pack ".data" :: B.ByteString
 
 defaultDarHeader = DarH { identifyDAR = cDAR_IDENTIFICATION
@@ -56,28 +56,6 @@ defaultDarHeader = DarH { identifyDAR = cDAR_IDENTIFICATION
 
 main = do
   args <- getArgs
-  darfile <- openFile (concat args) ReadMode
-
-  darIdent <- B.hGet darfile 3
-  darMaV <- B.hGet darfile 1
-  darMiV <- B.hGet darfile 1
-  darNoC <- B.hGet darfile 8
-  darChS <- B.hGet darfile 4
-  
-  let darHeader = DarH { identifyDAR = darIdent
-                       , majorV = fromIntegral $ head $ B.unpack darMaV :: Word8
-                       , minorV = fromIntegral $ head $ B.unpack darMiV :: Word8
-                       , nodeCount = fromIntegral $ head $ B.unpack darNoC :: Word64
-                       , checksum =  fromIntegral $ head $ B.unpack darChS :: Word32
-                       }
-  
-  if darHeader == defaultDarHeader
-    then putStrLn "Is Dar file."
-    else putStrLn $ "No Dar file." ++ (show darHeader) ++ "\n"
-         ++ (show cDAR_IDENTIFICATION) ++ (show cDAR_MAJOR_VERSION)
-         ++ (show cDAR_MINOR_VERSION)
-
-  hClose darfile
 
 
 
