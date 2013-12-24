803b7d5247a9c2322dab415c00ac43d1f2af149e
blockynight <joncpim@gmail.com>
2013-12-24 20:16:56 +0000

    Some command line options with a help message.

--- Main.hs
+++ Main.hs
@@ -18,25 +18,32 @@ main = do
   (action:args) <- getArgs
   
   case action of
+    "help" -> putStrLn helpText
     "test" -> B.writeFile (concat args) $ runPut $ putDARFile testDNodes
     "read" -> do
 
       darfile <- B.readFile $ concat args
 
-      let decodedDar = runGet getDARFile darfile
+      let decodedDar =  getDARFile darfile
 
-      case (checkDarHeader $ headerDAR decodedDar) of
-        True -> putStrLn $ (concat args) ++ " is TRUE DAR file."-- \n\nList of DataNodes:"
+      case decodedDar of
+        Right _ -> putStrLn $ (concat args) ++ " is TRUE DAR file."-- \n\nList of DataNodes:"
                 -- ++ (concatMap (\(DataN n d) -> unwords ["\nDataNode name:", BC.unpack n
                 --                                        , "\nDataLenght:", show $ B.length d
                 --                                        , "\nData:", BC.unpack d, "\n"])
                 --     $ dataNodes $ dataDAR decodedDar)
-        False -> putStrLn $ (concat args) ++ " is FALSE DAR file,"-- .data block possibly corrupt."
+        Left msg -> putStrLn $ (concat args) ++ " has an error:\n" ++ msg
 
 
 testDNodes = [ DataN (BC.pack "test") (BC.pack "This is the data section.\nHello again!")
              , DataN (BC.pack "hello") (BC.pack "A hello test, whats that?")
              ]
 
-
+helpText = unwords [ "Options:\n"
+                   , "help : For this help text.\n"
+                   , "pack <DAR file> <list of files>"
+                   ,  ": packs the files into a named DAR file.\n"
+                   , "unpack <DAR file> : unpack the files stored in the DAR file.\n"
+                   , "dump <DAR file>"
+                   , " : prints the contents on the DAR file to the screen.\n"]
 
