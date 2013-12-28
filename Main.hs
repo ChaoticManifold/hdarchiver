8d4e4f3faa31fd8722f3895d47820491a89498f7
blockynight <joncpim@gmail.com>
2013-12-28 12:56:58 +0000

    list option lists the name's of the DAR file's DataNodes using the index.

--- Main.hs
+++ Main.hs
@@ -17,15 +17,11 @@ main = do
   
   case option of
     "help" -> putStrLn helpText
-    "test" -> BC.writeFile (concat args) $ putDARFile testDNodes
     "pack" -> packDar darFile args
     "unpack" -> unpackDar darFile
     "dump" -> dumpDar darFile
+    "list" -> listDar darFile
     _ -> putStrLn "Invalid option, use 'help' for a list of options."
-
-testDNodes = [ DataN (BC.pack "test") (BC.pack "This is the data section.\nHello again!")
-             , DataN (BC.pack "hello") (BC.pack "A hello test, whats that?")
-             ]
 
 helpText = unwords [ "Options:\n"
                    , "help : For this help text.\n"
@@ -34,31 +30,23 @@ helpText = unwords [ "Options:\n"
                    , "To pack the contents of a directory replace"
                    , "unpack <DAR file> : unpack the files stored in the DAR file.\n"
                    , "dump <DAR file>"
-                   , " : prints the DataNodes of the DAR file to the screen.\n"]
-
-dumpDar :: FilePath -> IO ()
-dumpDar dFile = do
-  darfile <- BC.readFile dFile
-
-  case getDARFile darfile of 
-    Right dNodes -> putStrLn $ dFile ++ " is TRUE DAR file.\nContains:\n\n"
-                    ++ (concatMap (\d -> (BC.unpack $ identifyDNode d) ++ " \n"
-                                      ++ (BC.unpack $ contentDNode d) ++ "\n\n") dNodes)
-  
-    Left msg -> putStrLn $ dFile ++ " has an error:\n" ++ msg
+                   , " : prints the DataNodes of the DAR file to the screen.\n"
+                   , "list <DAR file> : lists the names of the DataNodes in the DAR file"
+                   ]
 
 packDar :: FilePath -> [FilePath] -> IO ()
-packDar dFile files@(d:_) = do
-  dirExists <- (length files == 1 &&) <$> doesDirectoryExist d
+packDar dFile files@(dir:_) = do
+  dirExists <- (length files == 1 &&) <$> doesDirectoryExist dir
   if dirExists
-    then do
---    putStrLn "Getting files from directory."
-    packDar dFile =<< (map (combine d)) <$>
-      filter (`notElem` [".", ".."]) <$> getDirectoryContents d
+    then packDar dFile =<< (map (combine dir)) <$>
+         filter (`notElem` [".", ".."]) <$> getDirectoryContents dir
     else do
---    putStrLn $ "Packing '" ++ (unwords files) ++ "' into " ++  dFile
     dContents <- mapM BC.readFile files
-    let dNodes = map (\(n,d) -> DataN (BC.pack n) d) $ zip (map takeFileName files) dContents
+
+    let dNodes = map mkdNode $ zip (map (BC.pack . takeFileName) files) dContents
+        mkdNode (n, d) = DataN (fromIntegral $ BC.length n) n
+                         (fromIntegral $ BC.length d) d
+
     BC.writeFile dFile $ putDARFile dNodes
 
 unpackDar :: FilePath -> IO ()
@@ -66,6 +54,29 @@ unpackDar dFile = do
   darfile <- BC.readFile dFile
 
   case getDARFile darfile of
-    Right dNodes -> mapM_ (\x -> BC.writeFile (BC.unpack $ identifyDNode x) $ contentDNode x) dNodes 
+    Right dNodes -> mapM_ (\x -> BC.writeFile (BC.unpack $ identifyDNode x)
+                                 $ contentDNode x) dNodes 
 
     Left msg -> putStrLn $ dFile ++ " has an error:\n" ++ msg
+
+dumpDar :: FilePath -> IO ()
+dumpDar dFile = do
+  darfile <- BC.readFile dFile
+
+  case getDARFile darfile of 
+    Right dNodes -> putStrLn $ dFile ++ "\n\n"
+                    ++ (concatMap (\d -> (BC.unpack $ identifyDNode d) ++ " \n"
+                                      ++ (BC.unpack $ contentDNode d) ++ "\n\n") dNodes)
+  
+    Left msg -> putStrLn $ dFile ++ " has an error:\n" ++ msg
+
+listDar :: FilePath -> IO ()
+listDar dFile = do
+  darfile <- BC.readFile dFile
+
+  case getDARIndex darfile of
+    Right iNodes -> putStrLn $ dFile ++ "\n\n"
+                    ++ (concatMap (\d -> (BC.unpack $ identifyINode d) ++ " \n") iNodes)
+
+    Left msg -> putStrLn $ dFile ++ " has an error:\n" ++ msg
+
